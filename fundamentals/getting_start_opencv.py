1ef3a97ef15250540352e916af61fc0e0bc0c5e7
conanttld <41601814+trobnguyen@users.noreply.github.com>
2019-09-27 15:21:29 +0900

    4th upload

--- fundamentals/getting_start_opencv.py
+++ fundamentals/getting_start_opencv.py
@@ -3,10 +3,15 @@
 import numpy as np 
 import cv2
 import os
+print(cv2.__version__)
 
 # find absolute file path
 #file_path = os.path.abspath("Future-human-Face.png")
-file_path = os.path.abspath("messi5.jpg")
+file_path = os.path.abspath("Future-human-Face.png")
+#file_path = os.path.abspath("image1.bmp")
+#file_path1 = "D://T_Robotics//Code//GitHub//OpenCV//image1.bmp"
+#print(file_path)
+#print(file_path1)
 
 
 """
@@ -16,15 +21,20 @@ cv2.IMREAD_UNCHANGED = -1   : Loads image as such including alpha channel
 """
 
 # Load an color image in grayscale
-#img = cv2.imread('Future-human-Face.png',0)
-img = cv2.imread('messi5.jpg',cv2.IMREAD_GRAYSCALE)      # read a specific image file in the working directory.
+img = cv2.imread('Future-human-Face.png',0)
+#img = cv2.imread("image1.bmp",1)
+#img = cv2.imread(file_path,1)
+#img = cv2.imread('messi5.jpg',cv2.IMREAD_GRAYSCALE)      # read a specific image file in the working directory.
+print(img)
+#height, width = img.shape[:2]
+#print('Image size: {}x{}'.format(height, width))
 
-cv2.namedWindow('image', cv2.WINDOW_AUTOSIZE)
+#cv2.namedWindow("image", cv2.WINDOW_AUTOSIZE)
 """
 cv2.WINDOW_AUTOSIZE : default flag
 cv2.WINDOW_NORMAL   : window is resizable, it will be helpful when image is too large in dimension and adding track bar to windows.
 """
-cv2.imshow('image',img)     # 'image' is the window name
+cv2.imshow("image",img)     # 'image' is the window name
 cv2.waitKey(0)              # wait until the key '0' is pressed
 cv2.destroyAllWindows()     # destroy all created windows
 """
